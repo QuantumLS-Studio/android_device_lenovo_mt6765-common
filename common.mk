8c37e1bba85e41551e45f1adfa3290b8a572f653
FTS427 <FTS427@outlook.com>
2024-12-14 22:43:56 +0800

    Update

--- common.mk
+++ common.mk
@@ -274,6 +274,13 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt6765 \
+    fstab.mt8768 \
+    fstab.mt6765.ramdisk \
+    fstab.mt8768.ramdisk
+
 PRODUCT_PACKAGES += \
     init.connectivity.rc \
     init.modem.rc \
